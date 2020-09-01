c1c03171b4f0da1a2509378d18f8c1427a3db572
Hardik Sachan <sachanhardik@gmail.com>
2020-09-01 19:23:45 +0530

    Templated Sign Up State GUI

---
+++ FSM/States/sign_up_state_gui.py
@@ -0,0 +1,24 @@
+from FSM.States.sign_up_state import SignUpState
+
+
+class SignUpStateGUI(SignUpState):
+    def input_sign_up(self):
+        # return super().input_sign_up()
+
+        # TODO : take input from gui
+
+        # return {
+        #     "first-name": first_name,
+        #     "last-name": last_name,
+        #     "add-line1": add_line1,
+        #     "add-line2": add_line2,
+        #     "city": city,
+        #     "state": state,
+        #     "pincode": pincode,
+        #     "password": password
+        # }
+        pass
+
+    def display_msg(self, msg):
+        # TODO: display msg array in msg box
+        pass
