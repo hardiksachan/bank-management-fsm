ea4437a5489d09a682521aed9b22d751c3107b88
Hardik Sachan <sachanhardik@gmail.com>
2020-09-01 08:23:30 +0530

    Switched to use a remote database (HelioHost)

--- connection.py
+++ connection.py
@@ -1,12 +1,20 @@
 import mysql.connector
 
 
-database_name = "bank_db"
+# database_name = "bank_db"
+database_name = "hsachan_bank_db"
 
+
+# con = mysql.connector.connect(
+#     host="localhost",
+#     username="local",
+#     password="MydB@125#",
+#     database=database_name
+# )
 
 con = mysql.connector.connect(
-    host="localhost",
-    username="local",
+    host="johnny.heliohost.org",
+    username="hsachan_local",
     password="MydB@125#",
     database=database_name
 )
