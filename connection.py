8766ebf85d6442c6fc3c44800b23636bac0ad8c6
Hardik Sachan <sachanhardik@gmail.com>
2021-03-14 21:21:51 +0530

    Completed Project

--- connection.py
+++ connection.py
@@ -1,21 +1,13 @@
 import mysql.connector
 
 
-# database_name = "bank_db"
-database_name = "hsachan_bank_db"
+database_name = "bank_db"
 
-
-# con = mysql.connector.connect(
-#     host="localhost",
-#     username="local",
-#     password="MydB@125#",
-#     database=database_name
-# )
 
 con = mysql.connector.connect(
-    host="johnny.heliohost.org",
-    username="hsachan_local",
-    password="MydB@125#",
+    host="localhost",
+    username="root",
+    password="MyNewPass",
     database=database_name
 )
 
